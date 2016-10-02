76fc9401a90eb0992e018f6e4646bc8a6974548d
Christopher Lee <hanernlee@gmail.com>
2016-10-02 23:02:19 +1100

    adding html page class

--- lib/batch_processor/camera_make.rb
+++ lib/batch_processor/camera_make.rb
@@ -8,13 +8,14 @@ module BatchProcessor
     end
 
     def add_model(model)
-      camera_model = @models.map { |current_model| current_model if current_model.name == model.name }.first
-
+      camera_model = @models.map do |current_model|
+        current_model if current_model.name == model.name
+      end.first
       camera_model ? model.images.map { |image| camera_model.add_image(image) } : @models << model
     end
 
     def images
-      @models.map(&:images).flatten
+      @models.map(&:images).flatten.take(10)
     end
   end
 end
---
+++ lib/batch_processor/html_page.rb
@@ -0,0 +1,29 @@
+require 'redbubble/template_factory'
+
+module BatchProcessor
+  class HTMLPage
+    def self.index(data,directory)
+      options = {title: "RedBubble Code Challenge", item: data.camera_makes, images: data.index_page_images.take(10), index: true }
+      index = TemplateFactory.new(options)
+      index.save(directory + "/index.html")
+    end
+
+    def self.makes(data,directory)
+      data.camera_makes.each do |camera_make|
+        file_name = camera_make.name.downcase.gsub(" ","_")
+        options = {title: camera_make.name, item: camera_make.models, images: camera_make.images  }
+        page = TemplateFactory.new(options)
+        page.save(directory + "/#{file_name}.html")
+      end
+    end
+
+    def self.models(data,directory)
+      data.models.each do |camera_model|
+        file_name = camera_model.name.downcase.gsub(" ","_")
+        options = { title: camera_model.name, item: camera_model.make, images: camera_model.images }
+        page = TemplateFactory.new(options)
+        page.save(directory + "/#{file_name}.html")
+      end
+    end
+  end
+end
